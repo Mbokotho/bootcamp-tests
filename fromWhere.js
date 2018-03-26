f86eb049b2cd5225141d7b5d0810f7f22b1f2576
mbokotho <mplwando@gmail.com>
2018-03-26 16:09:05 +0200

    Added bootcamp-tests

--- fromWhere.js
+++ fromWhere.js
@@ -1,11 +1,11 @@
 function fromWhere(regNum){
-  if(regNum == "CY"){
+  if(regNum.startsWith("CY")){
     return "Bellville"
      }
-   else if(regNum == "CJ"){
+   else if(regNum.startsWith("CJ")){
        return "Paarl"
      }
-   else if(regNum == "CA"){
+   else if(regNum.startsWith("CA")){
        return "Cape Town"
      }
    else{
--- test/fromWhere.tests.js
+++ test/fromWhere.tests.js
@@ -1,10 +1,10 @@
 describe('Fromwhere ', function(){
 
     it('should state the car is from cape town', function(){
-        assert.equal( fromWhere('CA'), 'Cape Town');
+        assert.equal( fromWhere('CA 2259'), 'Cape Town');
     });
     it('should state the car is from Paarl', function(){
 
-        assert.equal( fromWhere('CJ'),'Paarl');
+        assert.equal( fromWhere('CJ 2259'),'Paarl');
     });
 });
